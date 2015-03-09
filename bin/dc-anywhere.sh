a968aa5b306d7a3cfeaf1f66cf4a2b330efd8b91
Justin Carter <bodymindarts@online.de>
2015-03-09 10:30:42 +0100

    add dc-anywhere.sh

---
+++ bin/dc-anywhere.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while [ ! -f docker-compose.yml ] && [ "$PWD" != '/' ]; do
+    cd ..
+done
+
+if [ -f docker-compose.yml ]; then
+    docker-compose $@
+else
+    echo "Couldn't find docker-compose.yml in any parent directory" 1>&2
+    exit 1
+fi
